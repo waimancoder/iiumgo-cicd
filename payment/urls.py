173ba9391b72376250f765b55798440e8b1a16d4
waimancoder <waimancoder@gmail.com>
2023-03-25 03:08:42 +0800

    nc

--- payment/urls.py
+++ payment/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path("api/fpx-payment-methods/", views.FPXPaymentMethods.as_view(), name="fpx_payment_methods"),
     # path("process-selected-bank/", views.ProcessSelectedBank.as_view(), name="process_selected_bank"),
     path("api/fpx-payment", views.MakePayment.as_view(), name="fpx_payment"),
+    path("payment_return/", views.payment_return, name="payment_return"),
 ]
--- payment/views.py
+++ payment/views.py
@@ -1,11 +1,13 @@
 import os
 from django.shortcuts import render
+from django.urls import reverse
 from rest_framework import status
 from rest_framework import exceptions
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from .payment import get_adyen_client, get_payment_methods, get_fpx_banks, get_issuer_id, make_fpx_payment
 from django.http import HttpRequest
+from django.http import JsonResponse
 
 
 def get_current_domain(request: HttpRequest) -> str:
@@ -44,12 +46,11 @@ class FPXPaymentMethods(APIView):
 class MakePayment(APIView):
     def post(self, request):
         try:
-            domain = get_current_domain(request)
-            order_number = 1
+            order_number = 100
             amount = request.data["amount"]
             currency = request.data["currency"]
             issuer = request.data["issuer"]
-            return_url = f"http://{domain}:8000/"
+            return_url = request.build_absolute_uri(reverse("payment_return"))
 
             result = make_fpx_payment(order_number, amount, currency, issuer, return_url)
             result_dict = result.__dict__
@@ -57,7 +58,12 @@ class MakePayment(APIView):
             response_data = result_dict["message"]
 
             return Response(
-                {"success": True, "statusCode": status.HTTP_200_OK, "data": response_data}, status=status.HTTP_200_OK
+                {"success": True, "statusCode": status.HTTP_200_OK, "data": result_dict}, status=status.HTTP_200_OK
             )
         except Exception as e:
             return Response({"error": str(e)}, status=status.HTTP_400_BAD_REQUEST)
+
+
+def payment_return(request):
+    # Handle the payment return, e.g. save the payment result to the database
+    return JsonResponse({"status": "success", "message": "Payment processed successfully"})
