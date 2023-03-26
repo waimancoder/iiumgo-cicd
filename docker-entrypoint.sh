21bd7b70821c5a6f81b457af23f6173b959e639e
waimancoder <waimancoder@gmail.com>
2023-03-26 23:47:07 +0800

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -6,6 +6,6 @@ python manage.py makemigrations
 python manage.py migrate
 
 # Start Gunicorn and Daphne
-exec gunicorn mytaxi.wsgi:application --bind 0.0.0.0:8000 --workers 4 --timeout 120 & \ 
+exec gunicorn mytaxi.wsgi:application --bind 0.0.0.0:8000 --workers 4 --timeout 120 & \
      daphne -b 0.0.0.0 -p 9000 mytaxi.asgi:application --websocket_timeout 86400
 
